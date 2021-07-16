8274420e2c7c35f0f8028248c7dbed74e16079f7
Arth <arthrathod007@gmail.com>
2021-07-16 21:02:04 +0530

    update

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Hero from './components/Hero.js'
+import Service from './components/Service.js'
 import Nav from './components/Nav.js'
 import Heroo from './components/Heroo.js'
+import Gallery from './components/Gallery'
+import Testimonial from './components/Testimonial'
+import Location from './components/Location'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Heroo/>
-      <Hero/>
+      <Service name1="Threading" name2="Make Up" name3="Hair Treatment"  />
+      <Service name1="Manicure" name2="Pedicure" name3="Waxing"  />
+      <Service name1="Facials" name2="Rejuvenation Therapies" name3="Clean Up / Bleach"  />
+      <Testimonial/>
+      <Gallery/>
+      <Location/>
     </div>
   );
 }
